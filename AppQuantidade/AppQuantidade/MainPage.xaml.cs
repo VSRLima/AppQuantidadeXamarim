ab1d29af2d82a75f60272583288a469d281cf54c
Vinicius Reis <vinicius.reis@soterotech.com.br>
2022-03-03 21:42:01 -0300

    Finalizing first app on Xamarim

--- AppQuantidade/AppQuantidade/MainPage.xaml.cs
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -10,9 +10,26 @@ namespace AppQuantidade
 {
     public partial class MainPage : ContentPage
     {
+        int quantidade = 1;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            quantidade++;
+            QuantityLabel.Text = quantidade.ToString();
+        }
+
+        private void Diminuir(object sender, EventArgs e)
+        {
+            if(quantidade != 0)
+            {
+                quantidade--;
+                QuantityLabel.Text = quantidade.ToString();
+            }
+            
         }
     }
 }
